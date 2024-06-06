bae8c9f42845a529332dd9dd5b5eb4423c1d1884
gustavbarbosaa <gustavoaraujohab@gmail.com>
2024-06-06 13:06:36 -0300

    feat: implementa a primeira questao

---
+++ src/br/com/catolicapb/Main.java
@@ -0,0 +1,14 @@
+package br.com.catolicapb;
+
+import br.com.catolicapb.grafo.Grafo;
+
+public class Main {
+    public static void main(String[] args) {
+        Grafo grafo = new Grafo(4);
+        grafo.adicionarAresta(0,1);
+        grafo.adicionarAresta(1,2);
+        grafo.adicionarAresta(2,3);
+        grafo.removerAresta(1,2);
+        grafo.exibirGrafo();
+    }
+}
